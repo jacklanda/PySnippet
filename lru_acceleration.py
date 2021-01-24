c314d30092d5650b4f044c8df200e0a0f25b518c
jacklanda <yonyonlau@gmail.com>
2021-01-24 13:43:14 +0800

    add lru_acceleration

---
+++ lru_acceleration.py
@@ -0,0 +1,26 @@
+# use lru_cache to accelerate
+# your recursive calculation
+
+import sys
+import time
+from functools import lru_cache
+
+sys.setrecursionlimit(99999)
+
+
+@lru_cache(maxsize=1024)
+def feb(i: int):
+    if(i == 0):
+        return i
+    if(i == 1):
+        return 2
+    return feb(i-2) + feb(i-1)
+
+
+while(1):
+    start = time.perf_counter()
+    num = input("Please input a number to calculate: ")
+    print(feb(int(num)))
+    end = time.perf_counter()
+    print(f"continued time: {end - start}s")
+    print("")
