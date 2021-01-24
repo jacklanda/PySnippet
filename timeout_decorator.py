10c0eef001ff0b27f56ef8f795417e5d2f37df4a
jacklanda <yonyonlau@gmail.com>
2021-01-24 13:43:42 +0800

    add timeout_decorator

---
+++ timeout_decorator.py
@@ -0,0 +1,34 @@
+import time
+
+import timeout_decorator
+from timeout import timeout
+
+
+def main():
+    try:
+        info2()
+    except Exception as e:
+        print(e)
+
+    try:
+        info1()
+    except Exception as e:
+        print(e)
+
+    return
+
+
+@timeout_decorator.timeout(3)
+def info1():
+    while(True):
+        continue
+
+
+@timeout_decorator.timeout(3)
+def info2():
+    time.sleep(2)
+    print("func info2 didn't timeout")
+    return
+
+
+main()
